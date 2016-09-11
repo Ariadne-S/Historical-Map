e3652405d79026dcc768e800f3247d1d5212c8b1
Ariadne-S <toni.sefton@uq.net.au>
2016-09-11 21:07:39 +1000

    Got bubbles working
    We support lookups from 3 char country codes, to lat long.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@
 
 // Setup all our data
 
-var allEvent = null;
+var alpha2toLatLong = null;
+var codeMap = null;
+var allEvents = null;
 var timelineStart = 1789;
 var timelineEnd = 1914;
 
@@ -11,12 +13,42 @@ var results = Papa.parse("eventData.csv", {
 	download: true,
 	header: true,
 	skipEmptyLines: true,
+
 	complete: function(results) {
 		
-		populateTimeline(results.data);
-		
+		$.getJSON("countrycode-latlong-array.json", function(data) {
+			alpha2toLatLong = data;
+			
+			Papa.parse("code-map.csv", {
+				download: true,
+				header: true,
+				skipEmptyLines: true,
+
+				complete: function(mappingData) {
+					mappingData = mappingData.data;
+					
+					codeMap = {};
+					for (var i = 0; i < mappingData.length; i++) {
+						codeMap[mappingData[i].alpha3] = mappingData[i].alpha2;
+					}
+
+					// Events
+					populateTimeline(results.data);
+				}
+			});
+		});
 	}
 });
+
+function GetLocForCode(alpha3)
+{
+	var alpha2 = codeMap[alpha3];
+	var location = alpha2toLatLong[alpha2.toLowerCase()];
+	return {
+		latitude: parseFloat(location[0]),
+		longitude: parseFloat(location[1])
+	};
+}
 
 function parseDate(dateString) {
 	
@@ -53,7 +85,7 @@ function populateTimeline(data) {
 		data[i].id = i;
 	}
 	
-	allEvent = data;
+	allEvents = data;
 	
 	var timelineEvents = data.map(function (row) {
 		
@@ -66,7 +98,8 @@ function populateTimeline(data) {
 			"text": {
 				"headline": row.Title,
 				"text": "gfhjfghdfg"
-			}
+			},
+			"unique_id": row.id.toString()
 		}
 		
 		timelineEvent["start_date"] = parseDate(row.StartDate);
@@ -79,7 +112,7 @@ function populateTimeline(data) {
 		return timelineEvent;
 	});
 	
-	console.log(timelineEvents);
+	//console.log(timelineEvents);
 	updateTimeline(timelineEvents);
 }
 
@@ -100,7 +133,7 @@ function updateTimeline(timelineEvents) {
 	var additionalOptions = {
 		start_at_slide:     10,                            //OPTIONAL START AT SPECIFIC SLIDE
 		start_zoom_adjust:  4                            //OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
-		
+
 		//start_at_end: true,
 		//default_bg_color: {r:0, g:0, b:0},
 		//timenav_height: 250
@@ -109,11 +142,70 @@ function updateTimeline(timelineEvents) {
 	var timeline = new TL.Timeline('timeline-embed', timeline_json, additionalOptions);
 	
 	timeline.on("change", function(data) {
-		console.log(data);
+
+		var id = parseInt(data.unique_id);
+		var eventData = allEvents[id];
+		
+		displayEventOnMap(eventData);
 	});
 
 }
 
+function displayEventOnMap(eventData)
+{
+	var location = eventData.Location;
+	
+	var parts = location.split(";");
+	
+	var type = null;
+	if (parts.length == 1) {
+		type = "bubble";
+		
+		var loc = null;
+		var first = parts[0];
+		if (first.length == 3) {
+			loc = GetLocForCode(first);
+		} else {
+			var locParts = location.split(",");
+			loc = {
+				latitude: parseFloat(locParts[0]),
+				longitude: parseFloat(locParts[1])
+			};
+		}
+		
+		var radius = 10;
+		if (eventData.radius) {
+			radius = parseInt(eventData.radius);
+		}
+
+		map.bubbles([{
+			name: eventData.title,
+			latitude: loc.latitude,
+			longitude: loc.longitude,
+			radius: radius,
+			fillKey: 'pink'
+		}]);
+		
+	} else if (parts.length == 2) {
+		type = "arc";
+	} else if (parts.length > 2) {
+		type = "fill";
+	}
+
+	//1.2,12.1;AUD
+	
+	
+	//GetLocForCode
+	
+	map.arc([{
+		origin: {
+			country: "AUD"
+		},
+		destination: {
+			country: "USA"
+		}
+	}]);
+}
 
 // Draw stuff on the Screen
 
